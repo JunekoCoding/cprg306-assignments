1c5072afc1d42495e1caa997e9e3b686b511f16c
JunekoCoding <peter.gomez@edu.sait.ca>
2024-09-20 16:47:32 -0600

    Final changes
    Final changes to assignment 2

---
+++ app/week-2/student-info.js
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+export default function StudentInfo() {
+    return (
+      <main>
+        <h1>Student Information</h1>
+        <p>June Gomez</p>
+        <Link href="https://github.com/JunekoCoding/cprg306-assignments">GitHub Link</Link>
+      </main>
+    );
+    }
